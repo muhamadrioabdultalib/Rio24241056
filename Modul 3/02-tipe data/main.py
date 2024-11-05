643f0af698fcd1582af7bb56deff799e3774ae4c
Muhamad Rio Abdul Talib <160714376+muhamadrioabdultalib@users.noreply.github.com>
2024-11-05 12:04:24 +0800

    Add files via upload

---
+++ Modul 3/02-tipe data/main.py
@@ -0,0 +1,26 @@
+#a = 10, adalah variabel yang nilainya 10
+
+# integer (int)
+# tipe data : angka satuan yang tidak punya koma
+data_integer = 11
+print("data :",data_integer)
+# type()berfungsi untuk mengetahui tipe data variabel
+print("tipe data :",type (data_integer))#class int
+
+# float
+# tipe data : angka dengan koma
+data_float = 3.14
+print("data :", data_float)
+print("tipe data :", type (data_float))
+
+# string
+# tipe data : kumpulan karakter
+data_string =" universitas pendidikan mandalika "
+print("data :", data_string)
+print("tipe data :", type (data_string))
+
+#bolean
+#tipe data = : true/false
+data_boolean = False
+print("data :", data_boolean)
+print("tipe data :", type (data_boolean))
---
+++ Modul 3/03-castingTipeData/main.py
@@ -0,0 +1,30 @@
+#casting
+#mengubah tipe data ke tipe data lain
+#tipe data = int, float, str, bool
+
+#integer
+print("==INTEGER==")
+data_int = 9
+print('data :', data_int,"tipe = ",type (data_int))
+
+#ubah integer ke float
+data_float =float(data_int)
+#ubah integer ke string
+data_str = str(data_int)
+#ubah integer ke boolean
+data_bool = bool(data_int)
+#cetak
+print("data :",data_float, 'tipe', type(data_float))
+print("data :",data_str, 'tipe', (data_str))
+print("data :",data_bool, "type data",type(data_bool))
+
+#float                                          
+print("===FLOAT===")                                          
+data_float =3.14
+print("data :", data_float,"type data",type(data_float))
+
+#ubah menjadi integer
+
+#ubah menjadi boolean
+
+#ubah menjadi string
