35021c2446fc8f1156b1b4bda7a290b29f39b742
Muhamad Rio Abdul Talib <160714376+muhamadrioabdultalib@users.noreply.github.com>
2024-11-19 21:58:31 +0800

    Update hitungLuasLing.py

--- Modul 3/05-TPM02/hitungLuasLing.py
+++ Modul 3/05-TPM02/hitungLuasLing.py
@@ -1,10 +1,10 @@
 import math
 
-#minta input dari pengguna untuk jari jari lingkaran
+# Minta input dari pengguna untuk jari-jari lingkaran
 radius = float(input("masukkan jari-jari lingkaran : "))
 
-#menghitung luas lingkaran
+# Menghitung luas lingkaran
 area = math.pi * radius ** 2
 
-#menampilkan hasil
+# Menampilkan hasil
 print(f"luas lingkaran dengan jari-jari {radius} adalah {area:.2f}")
