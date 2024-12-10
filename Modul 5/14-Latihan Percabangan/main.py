e9bdebfdc849e8d1fc5d60b213c7051e820d6df2
Muhamad Rio Abdul Talib <160714376+muhamadrioabdultalib@users.noreply.github.com>
2024-12-10 23:37:23 +0800

    Add files via upload

---
+++ Modul 5/14-Latihan Percabangan/main.py
@@ -0,0 +1,10 @@
+#input total belanja
+total_belanja = float(input("Masukkan total belanja Rp.:"))
+
+# percabangan untuk menentukan belanja
+if total_belanja >= 500000:
+    print("Anda mendapatkan mouse dan keyboard.")
+else:
+    print("Anda hanya mendapatkan kunci.")
+    print("Anda hanya mendapatkan gantungan kunci.")
+print("terima kasih")
